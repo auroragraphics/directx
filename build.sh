aa94f2ca8ce8ce5cfa1952655186156df174b9eb
Adam Wilson <adamw@prospectivesoftware.com>
2014-06-29 10:37:16 -0700

    Improved build script.

--- build.sh
+++ build.sh
@@ -4,7 +4,18 @@ DEBUGOPTS32="-lib -m32 -debug -property -g -gs -gx -c -lib -version=DX11_1 -I../
 RELOPTS32="-lib -m32 -O -release -property -inline -c -lib -version=DX11_1 -I../.. -of../lib/libaurora_directx32.lib"
 SRCFILES="com.d dxgi.d d2d1.d dwrite.d config.d d3d11.d d3dx11.d package.d wic.d"
 
-dmd $RELOPTS64 $SRCFILES
-dmd $RELOPTS32 $SRCFILES
-dmd $DEBUGOPTS64 $SRCFILES
-dmd $DEBUGOPTS32 $SRCFILES
+printf "Building the Aurora DirectX x64 (Release) library ... "
+dmd $SRCFILES $RELOPTS64
+printf "Complete\n\r"
+
+printf "Building the Aurora DirectX x86 (Release) library ... "
+dmd $SRCFILES $RELOPTS32
+printf "Complete\n\r"
+
+printf "Building the Aurora DirectX x64 (Debug) library ... "
+dmd $SRCFILES $DEBUGOPTS64
+printf "Complete\n\r"
+
+printf "Building the Aurora DirectX x86 (Debug) library ... "
+dmd $SRCFILES $DEBUGOPTS32
+printf "Complete\n\r"
