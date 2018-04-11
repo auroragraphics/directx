e29e67352895f50417ac7fff317a43e87dd0a74a
Adam Wilson <adamw@ellipticbit.com>
2018-04-11 01:39:55 -0700

    Missed save.

--- d2d/package.d
+++ d2d/package.d
@@ -3,4 +3,4 @@ module aurora.directx.d2d;
 public import aurora.directx.d2d.d2d1_0;
 public import aurora.directx.d2d.d2d1_1;
 public import aurora.directx.d2d.d2d1_2;
-public import aurora.directx.d2d.d2d1_3;
+//public import aurora.directx.d2d.d2d1_3;
